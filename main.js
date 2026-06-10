b5a4b93f253b99ed1143834ac70e14e5d81b7005
curiousdave1472 <davidshanlay@gmail.com>
2026-06-10 02:19:06 +0000

    Changes

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 var fruits = ["apple", "banana", "cherry", "dragon fruit", "elderberry", "fig"]; 
 
 // Write your code below
+fruits[0] = "Apricot";
+console.log(fruits);
+
+fruits[3] = "Clementine";
+console.log(fruits);
+
+fruits.push("Grape");
+console.log(fruits);
