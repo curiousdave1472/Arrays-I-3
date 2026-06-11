9a5f7272943407640c1d62c773c472309e046b0f
curiousdave1472 <davidshanlay@gmail.com>
2026-06-11 22:18:53 +0000

    Updated

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 var fruits = ["apple", "banana", "cherry", "dragon fruit", "elderberry", "fig"]; 
 
 // Write your code below
+//Replace "Apricot" to Apple
 fruits[0] = "Apricot";
 console.log(fruits);
 
+// Replace Dragon Fruit with Clementine.
 fruits[3] = "Clementine";
 console.log(fruits);
 
+//Add Grapes to the end of array.
 fruits.push("Grape");
 console.log(fruits);
